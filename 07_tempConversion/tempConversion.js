c04e4857253fbcd4e3a68345078fef80ba431e1b
yof9 <149176377+yof9@users.noreply.github.com>
2024-06-26 19:54:51 +0300

    Fix

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheits) {
+  return Math.round(10 * (fahrenheits - 32) * 5 / 9) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  return Math.round(10 * (celsius * 9 / 5 + 32)) / 10;
 };
 
 // Do not edit below this line
