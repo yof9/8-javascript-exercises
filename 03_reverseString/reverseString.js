b8aa874677a840b2bffeb77723bdf359ee32b87e
yof9 <149176377+yof9@users.noreply.github.com>
2024-06-26 14:17:19 +0300

    Add reverse string functionality

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(txt) {
+    let reversedTxt = "";
+    for (const char of txt) {
+        reversedTxt = char + reversedTxt;
+    }
+    return reversedTxt;
 };
 
 // Do not edit below this line
