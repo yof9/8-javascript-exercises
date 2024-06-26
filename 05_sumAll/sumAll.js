31566da75881cb154b71d51115d9e329922ddb8b
yof9 <149176377+yof9@users.noreply.github.com>
2024-06-26 18:36:05 +0300

    Fix sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,11 @@
-const sumAll = function() {
-
-};
+const sumAll = function(a, b) {
+     
+    let sum = typeof a !== "number" || typeof b !== "number" || a < 0 || b < 0 ? 
+    "ERROR" : a <= b ?
+     (b - a + 1) * (a + b) / 2 :
+     (a - b + 1) * (a + b) / 2;
+    return sum;        
+};                                                               
 
 // Do not edit below this line
 module.exports = sumAll;
