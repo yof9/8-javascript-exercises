aea5bdf50f74d640ac28f6e363e88dc5c18fe0b4
yof9 <149176377+yof9@users.noreply.github.com>
2024-06-26 19:04:03 +0300

    Fix leapYears.js

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function(num) {
+    return (
+        (num % 4 === 0 && num % 100 !== 0) ||
+        (num % 4 === 0 && num % 100 === 0 && num % 400 === 0)
+    );
 };
 
 // Do not edit below this line
