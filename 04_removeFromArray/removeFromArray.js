d41fdf0195443264ed9de443e6a125fa13765275
yof9 <149176377+yof9@users.noreply.github.com>
2024-06-26 17:26:56 +0300

    Fix removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
 const removeFromArray = function() {
-};
+    
+    const args = [...arguments]
+    return args[0].filter((value) => !(args.slice(1).includes(value)));   
+
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
