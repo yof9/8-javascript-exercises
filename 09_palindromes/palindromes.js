2423f893033110152836e07b80fbe20cf2ae2708
yof9 <149176377+yof9@users.noreply.github.com>
2024-08-05 19:48:17 +0300

    Fix plaindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let arrayAlphaNumeric= str.split("").filter((char) => char.match(/[a-zA-Z0-9]/));
+    return arrayAlphaNumeric.slice(0, arrayAlphaNumeric.length/2).every((value, index) =>{
+        return value.toLowerCase() === arrayAlphaNumeric[arrayAlphaNumeric.length-1-index].toLowerCase()
+    });
 };
 
 // Do not edit below this line
