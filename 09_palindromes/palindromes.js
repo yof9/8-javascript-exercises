58d954eadb3207122ecbe55e5c604a7f86530e32
yof9 <149176377+yof9@users.noreply.github.com>
2024-08-05 19:53:19 +0300

    Add plaindromes.js comments

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
 const palindromes = function (str) {
+    // Create an array of viable chars of strings to be considered
     let arrayAlphaNumeric= str.split("").filter((char) => char.match(/[a-zA-Z0-9]/));
+
+    //slice left side of array and check all items counterpart on the right are the same
     return arrayAlphaNumeric.slice(0, arrayAlphaNumeric.length/2).every((value, index) =>{
         return value.toLowerCase() === arrayAlphaNumeric[arrayAlphaNumeric.length-1-index].toLowerCase()
     });
