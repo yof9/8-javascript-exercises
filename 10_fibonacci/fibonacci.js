1707bbd1efb45c411bb00b0621e05f9808cfd1e0
yof9 <149176377+yof9@users.noreply.github.com>
2024-08-05 20:32:05 +0300

    Fix fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = +n
+    if (n === 0) {return 0}
+    else if (n < 0) {return "OOPS"}
+    
+    let nth = preceding = 1;
+    for (i = 1; i < n-1; i++){
+        [preceding, nth] = [nth, nth+preceding];
+    }
+    return nth;
 };
 
 // Do not edit below this line
