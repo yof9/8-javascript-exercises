a7148bb9a199ff92504f8e48d93aa9f860ac338d
yof9 <149176377+yof9@users.noreply.github.com>
2024-08-05 16:50:24 +0300

    everything

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, item) => {return sum + item}, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, item) => total * item);;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let fact = 1;
+  while (a > 1) {
+    fact *= a;
+    a--;
+  }
+  return fact;
 };
 
 // Do not edit below this line
